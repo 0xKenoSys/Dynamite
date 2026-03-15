6ee0bd318413bcd5cf2165f6c0556913a3d614f1
0xKenoSys <294089357@qq.com>
2026-03-15 21:17:06 +0800

    feat: create v04.2 of SeaMount simulator

--- LoihiSeaMount_v04.py
+++ LoihiSeaMount_v04.py
@@ -91,6 +91,7 @@ for t in range(TIME_STEPS):
             current_phase = 2
         # 阶段二：蒸汽爆炸、火山口炸碎
         # 阶段三：造岛
+        # 给Rust和Java配环境
         # 1.岩浆流动不规则
         mask_fluid_state = temp_view > SOLIDIFY_TEMP
         flow_amount = (center_view - neighbors_avg) * 0.15 * mask_fluid_state
